9189b87f8d84e28f2f82164d8b9dd74ba414b1d7
Mitch Crowe <mitch.crowe@suite101.com>
2011-04-02 19:18:52 -0700

    Add rspec-rails to development group so that generators work. Add some versions to gems.

--- template.rb
+++ template.rb
@@ -8,10 +8,10 @@
 #--------------------------
 gem "ruby-debug", :group => [:development, :test]
 gem "awesome_print", :group => [:development, :test]
-gem "rspec-rails", :group => :test
-gem 'cucumber-rails', :group => :test
+gem "rspec-rails", "2.5.0", :group => [:development, :test]
+gem 'cucumber-rails', "0.4.0", :group => :test
 gem 'database_cleaner', :group => :test
-gem 'capybara', :group => :test
+gem 'capybara', "0.4.1.2", :group => :test
 gem "annotate-models", :group => :development
 gem 'metrical', :group => :development
 
@@ -19,7 +19,7 @@ gem 'metrical', :group => :development
 # FACTORY_GIRL
 #--------------------------
 gem "rails3-generators", :group => [:development, :test]
-gem "factory_girl_rails", :group => [:development, :test]
+gem "factory_girl_rails", "1.0.1", :group => [:development, :test]
 # Generate factories using the rails3-generator for factory-girl.
 generators = <<-GENERATORS
 
