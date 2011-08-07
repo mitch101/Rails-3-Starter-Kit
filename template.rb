7b4d1d68dfef1e4a5ea97e317952952c12c95e0c
Mitch Crowe <mitch.crowe@suite101.com>
2011-08-07 15:44:21 -0700

    Add rspec-rails to development environment so that generators and rake tasks work properly. Generate request specs.

--- template.rb
+++ template.rb
@@ -4,31 +4,41 @@
 APP_NAME = app_name
 RUBY_NAME = "ruby-1.9.2-p290"
 RUBY_GEMSET_NAME = "#{RUBY_NAME}@#{APP_NAME}"
-TEMPLATE_PATH = "https://raw.github.com/mitch101/Rails-3-Starter-Kit/master/template/"
+TEMPLATE_PATH = File.expand_path('../', __FILE__)
 
-def get_template_file(template_file, destination = template_file)
-  remove_file destination
-  get TEMPLATE_PATH + template_file, destination
+if yes?("\r\nInstall with postgres?")
+  # INSTALL WITH POSTGRES
+  POSTGRES_PASS = ask("\r\nEnter your postgres password:")
+  USE_POSTGRES = true
+  gem 'pg'
+else
+  USE_POSTGRES = false
 end
 
-# Run a command in the scope of the new gemset for this project.
-# (Otherwise it will use the wrong rubies.)
-def run_in_gemset(command)
-  run "rvm #{RUBY_GEMSET_NAME} -S #{command}"
-end
+# COPY TEMPLATE FILES INTO NEW PROJECT
+directory "#{TEMPLATE_PATH}/template", "." , :force => true
+
+# CLEANUP SOME SILLY DEFAULT RAILS FILES
+remove_file 'public/index.html'
+remove_file 'public/images/rails.png'
+remove_file 'app/views/layouts/application.html.erb'
+
+# KEEP LOG & TMP DIRECTORIES IF EMPTY
+create_file 'log/.gitkeep'
+create_file 'tmp/.gitkeep'
 
 # CONFIGURE GENERATORS
 #   Don't generate stylesheets when scaffolding.
 #   Generate factories using the rails3-generator for factory-girl.
 #   Don't generate specs for views, controllers, helpers, 
-#   requests, or routes in scaffolding.
+#   or routes in scaffolding.
 generators = <<-GENERATORS
 
     config.generators do |g|
       g.stylesheets false
       g.template_engine :haml
       g.test_framework :rspec
-      g.request_specs false
+      g.request_specs true
       g.view_specs false
       g.controller_specs false
       g.helper_specs false
@@ -38,88 +48,23 @@ generators = <<-GENERATORS
 GENERATORS
 application generators
 
-# SETUP RVM TO USE/CREATE PROJECT SPECIFIC GEMSET WHEN CD INTO PROJECT
-# get_template_file ".rvmrc"
-# gsub_file '.rvmrc', '[ruby_name]', "#{RUBY_NAME}"
-# gsub_file '.rvmrc', '[ruby_gemset_name]', "#{RUBY_GEMSET_NAME}"
-
-# REPLACE THE DEFAULT GEMFILE
-# get_template_file "Gemfile"
-# 
-# # CREATE HAML APPLICATION LAYOUT
-# #   Replace the erb application layout with haml one.
-# #   Include jquery hosted by google.
-# #   Include underscore.js hosted by cdnjs
-# #   Include compass generated css.
-# get_template_file "app/views/layouts/application.html.haml"
-# 
-# get_template_file ".gitignore"
-# 
-# # INSTALL COMPASS
-# # Create compass config file.
-# get_template_file "config/compass.rb"
-# # Setup default sass files.
-# get_template_file "app/stylesheets/ie.scss"
-# get_template_file "app/stylesheets/print.scss"
-# get_template_file "app/stylesheets/application.scss"
-# get_template_file "app/stylesheets/partials/_base.scss"
-# # Include a styleguide accessible at /styleguide.html
-# get_template_file "public/styleguide.html"
-# 
-# # CREATE GUARDFILE
-# get_template_file "Guardfile"
-# 
-# get_template_file ".rspec"
-# get_template_file "spec/spec_helper.rb"
-# get_template_file "features/support/env.rb"
-# get_template_file "config/cucumber.yml"
-
-directory(TEMPLATE_PATH, 'template')
-
-# CREATE GEMSET FOR APP
+# CREATE A GEMSET FOR THIS PROJECT
 run "rvm --create #{RUBY_GEMSET_NAME}"
 
-# if yes?("\r\nInstall with postgres?")
-#   # INSTALL POSTGRES
-#   # Create a database.yml for postgres
-#   postgres_pass = ask("\r\nEnter your postgres password:")
-#   remove_file "config/database.yml"
-#   get_template "config/database.yml"
-#   gsub_file 'config/database.yml', '[app_name]',"#{APP_NAME}"
-#   gsub_file 'config/database.yml', '[postgres_pass]',"#{postgres_pass}" 
-#   # Add pg gem.
-#   gem 'pg'
-#   create_databases = true
-# else
-#   create_databases = false
-# end
-
-# CLEANUP SOME SILLY DEFAULT RAILS FILES
-remove_file 'public/index.html'
-remove_file 'public/images/rails.png'
+# Run a command in the scope of the new gemset for this project.
+# (Otherwise it will use the wrong rubies.)
+def run_in_gemset(command)
+  run "rvm #{RUBY_GEMSET_NAME} -S #{command}"
+end
 
 # INSTALL GEMS
-#   Note: We generate bin stubs so that we don't need to use
-#   'bundle exec' before gem executables. You should add
-#   './bin' to your path so that you can run 'rake', 'cucumber'
-#   etc from the Gemfile with ease.
-#   See http://yehudakatz.com/2011/05/30/gem-versioning-and-bundler-doing-it-right/
-# run_in_gemset "gem install bundler"
-# run_in_gemset "bundle install --binstubs"
+run_in_gemset "gem install bundler"
+run_in_gemset "bundle install"
 
-# SETUP RSPEC AND CUCUMBER TO WORK WITH SPORK
-# run_in_gemset 'rails g cucumber:install'
-
-# CREATE DATABASES
-# if create_databases
-#   run_in_gemset "rake db:create:all"
-# end
+# CREATE POSTGRES DATABASES
+run_in_gemset "rake db:create:all" if USE_POSTGRES
 
 # CONFIGURE GIT AND INITIALIZE A REPOSITORY
-# Create a standard .gitignore file.
-create_file 'log/.gitkeep'
-create_file 'tmp/.gitkeep'
-# Initialize a git repository.
 git :init
 git :add => '.'
 git :commit => "-a -q -m 'Initial commit'"
